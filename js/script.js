dd8f745ec515f1862d3e2b22c46d33f4b2a23021
Anantharaj M <75241794+Anantharajdeveloper@users.noreply.github.com>
2024-06-27 16:45:29 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,14 @@
+
+
+
+
+
+
+const typed = new Typed('.multiple_text',{
+    strings: ['Web Developer','Frontend Developer', 'Wep Designer'],
+    typeSpeed: 80,
+    backSpeed:80,
+    backDelay:1200,
+    loop: true,
+
+  });
